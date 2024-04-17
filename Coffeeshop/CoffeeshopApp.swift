44ea89f2a825c2150937b58a40f9c0bd216d7de6
Dicky-Fahriza <dickyfahriza46@gmail.com>
2024-04-17 14:12:53 +0700

    Initial Commit

---
+++ Coffeeshop/CoffeeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeshopApp.swift
+//  Coffeeshop
+//
+//  Created by MacBook Pro on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
